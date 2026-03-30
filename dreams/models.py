71c155a7927e4a0cffe5dde0cf62e4419c9cc49f
Yan Naing <yannaing.dev@gmail.com>
2026-03-30 13:29:05 +0630

    Updating repository with post feature renamed

---
+++ dreams/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Dream(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
