2de0b3849040663fd89d6ce9fc22efc201a4a382
Yan Naing <yannaing.dev@gmail.com>
2026-03-30 15:20:25 +0630

    feat: Like model added. fix: fix some error with migrations while renaming

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from dreams.models import Dream
+
 # Create your models here.
 class User(AbstractUser):
-    dream = models.ManyToManyField(Dream, blank=True)
+    dream = models.ManyToManyField("dreams.Dream", blank=True)
     first_name = models.CharField(max_length=25, null=False, blank=False)
     last_name = models.CharField(max_length=25, null=False, blank=False)
     username = models.CharField(max_length=25, null=True, blank=True, unique=True)
--- feeds/admin.py
+++ feeds/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Feed
+from .models import Feed, Like
 # Register your models here.
 admin.site.register(Feed)
+admin.site.register(Like)
