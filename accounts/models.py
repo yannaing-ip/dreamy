42d62ee98fcbf369c1bb28a2fe4cb0d3e70f83eb
Yan Naing <yannaing.dev@gmail.com>
2026-03-30 13:27:53 +0630

    Add account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from dreams.models import Dream
 # Create your models here.
 class Account(AbstractUser):
+    dream = models.ManyToManyField(Dream, blank=True)
     first_name = models.CharField(max_length=25, null=False, blank=False)
     last_name = models.CharField(max_length=25, null=False, blank=False)
+    username = models.CharField(max_length=25, null=True, blank=True, unique=True)
     email = models.CharField(max_length=75, null=False, blank=False, unique=True)
     is_active = models.BooleanField(default=True)
     join_at = models.DateTimeField(auto_now_add = True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["first_name", "last_name"]
+    REQUIRED_FIELDS = ["first_name", "last_name", "username"]
 
     def __str__(self):
-        return f"{first_name} {last_name}"
+        return f"{self.first_name} {self.last_name}"
