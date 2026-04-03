c112d1ed790c5db613630c367f15edf072678eaa
Yan Naing <yannaing.dev@gmail.com>
2026-04-03 14:43:40 +0630

    Add MeSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
+from dreams.serializers import DreamSerializer
 
 User = get_user_model()
+
+class MeSerializer(serializers.ModelSerializer):
+    dream = DreamSerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = [
+                "id",
+                "username",
+                "email",
+                "first_name",
+                "last_name",
+                "dream"
+                ]
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
