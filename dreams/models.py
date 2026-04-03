2bf7fe9655da9d5c497b7b64839cbff87e2defe2
Yan Naing <yannaing.dev@gmail.com>
2026-04-03 10:42:33 +0630

    Add slug field in dream model

--- dreams/models.py
+++ dreams/models.py
@@ -1,17 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
 class Dream(models.Model):
-    name = models.CharField(
-            max_length=100, 
-            null=False,
-            blank=False,
-            unique=True
-        )
-    description = models.TextField(
-            null=True,
-            blank=True
-        )
+
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
