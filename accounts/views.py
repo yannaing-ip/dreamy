944d8d960f5ad2966a7e0244751615ff00a2114a
Yan Naing <yannaing.dev@gmail.com>
2026-04-03 08:28:38 +0630

    -Add url path for login and register -Add login view and register view -Implement JWT authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.views import APIView
+from .models import User
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import login
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+class RegisterView(CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {"message": "User registered successfully"},
+            status=status.HTTP_201_CREATED
+        )
+
+class LoginView(GenericAPIView):
+
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        refresh_token = RefreshToken.for_user(user)
+
+        return Response({
+            "access":f"{refresh_token.access_token}",
+            "refresh":f"{refresh_token}"
+            })
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
